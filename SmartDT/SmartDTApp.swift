52f1228397e2c967b901827c798136f4b5f27403
maynemei <maynemei@umich.edu>
2024-01-22 15:37:48 -0500

    Initial Commit

---
+++ SmartDT/SmartDTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartDTApp.swift
+//  SmartDT
+//
+//  Created by enpei mei on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartDTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
